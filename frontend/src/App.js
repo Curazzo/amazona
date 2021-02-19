29411ffea35b8a56349bbf042a67ec77720e322d
curazzo <curazzo@gmail.com>
2021-02-19 19:56:29 +0100

    shop

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
               <i className="fa fa-bars"></i>
             </button>
             <Link className="brand" to="/">
-              CULT Automotive
+              CULT Automotive Shop
             </Link>
           </div>
           <div>

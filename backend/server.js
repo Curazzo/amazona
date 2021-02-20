c12cb44822071f2f050490f5134b5243aabb95fc
curazzo <curazzo@gmail.com>
2021-02-20 17:54:35 +0100

    fastlify01

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 import orderRouter from './routers/orderRouter.js';
 import uploadRouter from './routers/uploadRouter.js';
+import fast from 'fastify'
+const fastify = fast({ logger: true })
 
 dotenv.config();
 
@@ -42,7 +44,18 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+fastify.addHook('onRequest', async (request, reply) => {
+  if (request.headers['x-forwarded-proto']) {
+      if (request.headers['x-forwarded-proto'] === 'http') {
+          return reply.redirect(`https://${request.headers.host}${request.raw.url}`)
+      }
+  }
+})
+
 const port = process.env.PORT || 5000;
+
+fastify.listen(port, '0.0.0.0')
+
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
